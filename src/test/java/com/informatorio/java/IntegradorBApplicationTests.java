d727bc6b9b07ad79da1a7cb487954caf92f9dcee
Darian Hiebl <dahiebl@gmail.com>
2023-12-07 13:59:44 -0300

    Arrancando estructura Spring

---
+++ src/test/java/com/informatorio/java/IntegradorBApplicationTests.java
@@ -0,0 +1,13 @@
+package com.informatorio.java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntegradorBApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
